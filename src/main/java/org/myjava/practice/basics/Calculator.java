5891bb5a5ef846e8fc17cea7a79ae9b3baaa26e8
Sobhan369 <sobhanbabukarri@gmail.com>
2025-07-10 22:42:11 +0530

    updated

--- src/main/java/org/myjava/practice/basics/Calculator.java
+++ src/main/java/org/myjava/practice/basics/Calculator.java
@@ -4,26 +4,28 @@ import java.util.Scanner;
 
 public class Calculator {
     public void add(int a, int b){
-        System.out.println(a+b);
+        int t = a+b;
+        System.out.println(" addition "+ t);
     }
     public void mul(int a, int b){
-        System.out.println(a*b);
+        System.out.println("multiplicatin  "+a*b);
     }
     public void sub(int a, int b){
-        System.out.println(a-b);
+        System.out.println( a-b  +" subtraction");
     }
     public void div(int a,int b){
-        System.out.println(a/b);
+        System.out.println("division "+a/b);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the a b values");
         int a = scanner.nextByte();
         int b = scanner.nextByte();
         Calculator calculator = new Calculator();
         calculator.add(a,b);
-     //   calculator.sub(a,b);
-    //    calculator.mul(a,b);
-    //    calculator.div(a,b);
+        calculator.sub(a,b);
+        calculator.mul(a,b);
+        calculator.div(a,b);
     }
 }
