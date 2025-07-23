6612321177f4973758c82e44f704c0db4592e4bd
Sobhan369 <sobhanbabukarri@gmail.com>
2025-07-23 15:22:37 +0530

    updated

--- src/test/java/com/myjava/string/test/StringArrayTest.java
+++ src/test/java/com/myjava/string/test/StringArrayTest.java
@@ -4,7 +4,6 @@ import com.myjava.string.java.ArrayIndexClass;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
-
 public class StringArrayTest {
     @Test
     public void arrayTest(){
@@ -13,28 +12,21 @@ public class StringArrayTest {
         int index = 2;
         int result =   arrayIndexClass.number(numbers,index);
         Assertions.assertEquals(4,result);
-
         index=3;
         result= arrayIndexClass.number(numbers,index);
         Assertions.assertEquals(6,result);
-
         index=7;
         try {
             result=arrayIndexClass.number(numbers,index);
             Assertions.assertEquals(4,index);
             Assertions.fail();
-
         }
         catch (ArrayIndexOutOfBoundsException e){
             Assertions.assertNotNull(e);
             Assertions.assertEquals("Index out of bound",e.getMessage());
     //        throw new ArrayIndexOutOfBoundsException();
         }
-
-
-
     }
-
 }
 
 
