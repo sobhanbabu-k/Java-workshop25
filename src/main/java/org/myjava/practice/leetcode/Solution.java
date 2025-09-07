92e6e9363825aec32dfe75dd6bf8f9f5ef48c2a1
Sobhan369 <sobhanbabukarri@gmail.com>
2025-09-07 21:39:59 +0530

    updated

--- src/main/java/org/myjava/practice/leetcode/Solution.java
+++ src/main/java/org/myjava/practice/leetcode/Solution.java
@@ -26,6 +26,24 @@ class Solution {
         return node;
     }
 
+    private ListNode addTwoNumgbersRecursive(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null && carry == 0) {
+            return null;
+        }
+
+        int val1 = (l1 != null) ? l1.val : 0;
+        int val2 = (l2 != null) ? l2.val : 0;
+        int sum = val1 + val2 + carry;
+
+        ListNode node = new ListNode(sum % 10);
+        int newCarry = sum / 10;
+
+        ListNode next1 = (l1 != null) ? l1.next : null;
+        ListNode next2 = (l2 != null) ? l2.next : null;
+
+        return node;
+    }
+
     public static void main(String[] args) {
         ListNode listNode = new ListNode(3,new ListNode(6,new ListNode(9)));
         ListNode lt = new ListNode(3,new ListNode(6,new ListNode(6)));
@@ -79,6 +97,8 @@ class Solution {
     }
 
 }
+
+
 
 
 
