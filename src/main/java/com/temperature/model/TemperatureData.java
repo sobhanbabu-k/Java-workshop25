22b440ee345ec570cbb8719a0034892113092584
Sobhan369 <sobhanbabukarri@gmail.com>
2025-09-23 21:37:20 +0530

    updated

--- src/main/java/com/temperature/model/TemperatureData.java
+++ src/main/java/com/temperature/model/TemperatureData.java
@@ -1,4 +1,10 @@
 package com.temperature.model;
 
+import com.jmh.DemoJmh;
+
 public record TemperatureData() {
+    public static void main(String[] args) {
+        DemoJmh.addItem();
+        DemoJmh.addElement();
+    }
 }
